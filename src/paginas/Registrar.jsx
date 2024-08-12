2c36462523167b1ce1fdbe913ffb8b5314bef122
Ezequiel Lino <sonisombra@gmail.com>
2024-08-11 23:29:05 -0600

    mostramos mensaje de validacion

--- src/paginas/Registrar.jsx
+++ src/paginas/Registrar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Alerta } from "../components/Alerta";
 
 export const Registrar = () => {
 
@@ -8,25 +9,30 @@ export const Registrar = () => {
   const [ password, setPassword ]= useState('');
   const [ repetirPassword, setRepetirPassword ]= useState('');
 
+  const [ alerta, setAlerta ] = useState({});
+
   const handleSubmit = e => {
     e.preventDefault();
 
     if( [nombre, email, password, repetirPassword ].includes('')){
-      console.log('Todos los campos son obligatorios');
+      setAlerta({ msg:'Todos los campos son obligatorios', error: true})
       return
     }
 
     if(password !== repetirPassword){
-      console.log('Las contraseñas no coinciden');
+      setAlerta({ msg: 'Las contraseñas no coinciden', error: true });
       return
     }
 
     if(password.length < 6) {
-      console.log('La contraseña debe tener al menos 6 caracteres');
+      setAlerta({ msg: 'La contraseña debe tener al menos 6 caracteres', error: true});
       return 
     }
 
+    setAlerta({})
   }
+
+  const { msg } = alerta;
 
   return (
     <>
@@ -38,6 +44,12 @@ export const Registrar = () => {
       </div>
 
       <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+        
+        { msg && <Alerta
+            alerta={alerta}
+          />
+        }
+
         <form onSubmit={ handleSubmit }>
           <div className="my-5">
             <label 
