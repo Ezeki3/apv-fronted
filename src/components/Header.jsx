2a0ef1b8d7988db9ddf66e96cd29dc3649707739
Ezequiel Lino <sonisombra@gmail.com>
2024-10-06 21:47:03 -0600

    cerrar la sesion del usuario

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 export const Header = () => {
+
+  const { cerrarSesion} = useAuth();
+
   return (
     <header className="py-10 bg-indigo-600">
       <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center">
@@ -15,7 +19,8 @@ export const Header = () => {
 
           <button
             type="button" 
-            className="text-white text-sm uppercase font-bold">
+            className="text-white text-sm uppercase font-bold"
+            onClick={cerrarSesion}>
             Cerrar Sesión
           </button>
         </nav>
