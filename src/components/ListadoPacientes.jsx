c12a1a80d388756fd36cdddc62de6953a0ebe911
Ezequiel Lino <sonisombra@gmail.com>
2025-01-22 23:33:46 -0600

    mostrar de forma condicional si se ven los pacientes o no

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import usePacientes from "../hooks/usePacientes";
 
 export default function ListadoPacientes() {
+
+  const {pacientes } = usePacientes();
+
+  console.log(pacientes)
   return (
-    <h1>ListadoPacientes</h1>
+    <>
+      { pacientes.length ? 
+        (
+          <>
+            <h2 className="font-block text-3xl text-center">Listado Pacientes</h2>
+
+            <p className="text-xl mt-5 mb-10 text-center">Administra tus {''}
+              <span className="text-indigo-600 font-bold"> Pacientes y Citas</span>
+            </p>
+          </>
+        ) : 
+        (
+          <>
+            <h2 className="font-block text-3xl text-center">No hay pacientes</h2>
+
+            <p className="text-xl mt-5 mb-10 text-center">Comienza agregando pacientes {''}
+              <span className="text-indigo-600 font-bold"> y apareceran en este lugar</span>
+            </p>
+          </>
+        )
+      }
+    </>
   )
 }
