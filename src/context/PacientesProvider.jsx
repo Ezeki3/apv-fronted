1f20fe547ef6b21f6503bf62def6736b188fb16d
Ezequiel Lino <sonisombra@gmail.com>
2025-04-17 00:09:55 -0600

    eliminando pacientes

--- src/context/PacientesProvider.jsx
+++ src/context/PacientesProvider.jsx
@@ -48,7 +48,7 @@ export const PacientesProvider = ({children}) => {
         const { data } = await clienteAxios.put( `/pacientes/${paciente.id}`, paciente, config)
         const pacientesActualizado = pacientes.map( pacienteState => pacienteState._id === data._id ? data : pacienteState )
         setPacientes(pacientesActualizado)
-        
+
       } catch (error) {
         console.log(error);
       }
@@ -73,13 +73,36 @@ export const PacientesProvider = ({children}) => {
     setPaciente(paciente)
   }
 
+  const eliminarPaciente = async id => {
+    const confirmar = confirm('¿Confirma que deseas eliminar');
+    if (confirmar) {
+      try {
+        const token = localStorage.getItem('token')
+        const config = {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          }
+        }
+
+        const  {data} = await clienteAxios.delete(`/pacientes/${id}`, config);
+        const pacientesActualizado = pacientes.filter(pacienteState => pacienteState._id !== id)
+        setPacientes(pacientesActualizado)
+        
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    
+  }
   return (
     <PacientesContext.Provider
       value={{
         pacientes,
         guardarPaciente,
         setEdicion,
-        paciente
+        paciente,
+        eliminarPaciente
       }}
     >
       {children}
