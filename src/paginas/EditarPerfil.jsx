771d6d42de2f92f3e831be2b48db75e05f10db60
Ezequiel Lino <sonisombra@gmail.com>
2025-04-17 23:02:43 -0600

    añadiendo la informacion en los campos

--- src/paginas/EditarPerfil.jsx
+++ src/paginas/EditarPerfil.jsx
@@ -1,6 +1,16 @@
+import { useState, useEffect } from "react";
 import { AdminNav } from "../components/AdminNav";
+import useAuth from "../hooks/useAuth";
 
 export const EditarPerfil = () => {
+
+  const { auth } = useAuth();
+  const [perfil, setPerfil] = useState({});
+  
+  useEffect( () => {
+    setPerfil(auth)
+  }, [auth])
+  
   return (
     <>
       <AdminNav />
@@ -18,6 +28,12 @@ export const EditarPerfil = () => {
                   type="text" 
                   className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
                   name="nombre"
+                  value={perfil.nombre || ''}
+                  onChange = {  (e) => setPerfil( {
+                      ...perfil, 
+                      [e.target.name]: e.target.value
+                    })
+                  }
                 />
               </div>      
 
