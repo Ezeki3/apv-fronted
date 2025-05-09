c48360cb54005d4575c7ba4d0c7dbecc17a2e820
Ezequiel Lino <sonisombra@gmail.com>
2025-05-09 00:08:44 -0600

    cargar los pacientes del usuario autenticado

--- src/layout/RutaProtegida.jsx
+++ src/layout/RutaProtegida.jsx
@@ -6,7 +6,6 @@ import useAuth from '../hooks/useAuth';
 export const RutaProtegida = () => {
 
   const {auth, cargando} = useAuth();
-  console.log(auth)
   if (cargando) return 'Cargando...';
   
   return (
